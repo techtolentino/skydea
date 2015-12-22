290f7557768a69fd6f61d60c08a32d7b90db37f2
Randy Tolentino <makertolentino@gmail.com>
2015-12-22 05:14:40 -0600

    Working remotely

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,8 @@
-import config from '../config/environment';
 import Ember from 'ember';
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 const { inject } = Ember;
 
 export default FirebaseAdapter.extend({
-  firebase: new Firebase(config.firebase)
+  firebase: inject.service(),
 });
